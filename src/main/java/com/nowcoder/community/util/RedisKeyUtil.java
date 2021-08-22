53f2d32168bcfa6150c4535cf0183ecd7019b9b6
wsyconan <wsyconan@gmail.com>
2021-08-22 17:13:35 +0300

    关注功能

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -8,6 +8,10 @@ public class RedisKeyUtil {
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
 
     private static final String PREFIX_USER_LIKE = "like:user";
+    // 被关注者
+    private static final String PREFIX_FOLLOWEE = "followee";
+    // 关注者
+    private static final String PREFIX_FOLLOWER = "follower";
 
     // 某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
@@ -19,6 +23,18 @@ public class RedisKeyUtil {
     // like:user:userId -> int
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+    // 某个用户关注的实体(根据关注时间的有序集合)
+    // followee:userId:entityType -> zset(entityId,time)
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // 某个实体拥有的粉丝
+    // follower:entityType:entityId -> zset(userId,time)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
     }
 
 }
